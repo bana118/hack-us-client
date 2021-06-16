85ce17905f801d783ffdb03cf7e836fb6c43a6a9
bana118 <bana.titech@gmail.com>
2021-06-16 11:06:10 +0000

    add login dialog

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import Link from "next/link";
 import {
   AppBar,
@@ -9,51 +9,67 @@ import {
   Grid,
 } from "@material-ui/core";
 import { Global } from "@emotion/react";
+import { LoginDialog } from "../components/SimpleDialog";
 
 type LayoutProps = {
   children?: ReactNode;
 };
 
-const Layout = ({ children }: LayoutProps): JSX.Element => (
-  <div>
-    <Global
-      styles={{
-        body: {
-          margin: 0,
-        },
-      }}
-    />
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-          Hack Us
-        </Typography>
-        {/* TODO ログイン or ログアウト 切り替え */}
-        <Button color="inherit">Login</Button>
-      </Toolbar>
-    </AppBar>
-    <Container>{children}</Container>
-    <Container>
-      <footer>
-        <hr />
-        <Grid container justifyContent="center" textAlign="center" spacing={6}>
-          <Grid item xs={12} md="auto">
-            <Link href="/">
-              <a>トップページ</a>
-            </Link>
+const Layout = ({ children }: LayoutProps): JSX.Element => {
+  const [loginDialogOpen, setLoginDialogOpen] = useState(false);
+
+  return (
+    <div>
+      <Global
+        styles={{
+          body: {
+            margin: 0,
+          },
+        }}
+      />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+            Hack Us
+          </Typography>
+          {/* TODO ログイン or ログアウト 切り替え */}
+          <Button color="inherit" onClick={() => setLoginDialogOpen(true)}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Container>{children}</Container>
+      <Container>
+        <footer>
+          <hr />
+          <Grid
+            container
+            justifyContent="center"
+            textAlign="center"
+            spacing={6}
+          >
+            <Grid item xs={12} md="auto">
+              <Link href="/">
+                <a>トップページ</a>
+              </Link>
+            </Grid>
+            <Grid item xs={12} md="auto">
+              <Link href="/privacy">
+                <a>プライバシーポリシー</a>
+              </Link>
+            </Grid>
+            <Grid item xs={12} md="auto">
+              <a href="github.com/bana118/hack-us-client">Github</a>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md="auto">
-            <Link href="/privacy">
-              <a>プライバシーポリシー</a>
-            </Link>
-          </Grid>
-          <Grid item xs={12} md="auto">
-            <a href="github.com/bana118/hack-us-client">Github</a>
-          </Grid>
-        </Grid>
-      </footer>
-    </Container>
-  </div>
-);
+        </footer>
+      </Container>
+      <LoginDialog
+        open={loginDialogOpen}
+        onClose={() => setLoginDialogOpen(false)}
+      />
+    </div>
+  );
+};
 
 export default Layout;
