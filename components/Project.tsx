bde19114e56ab273cbc395cb54ff882666221870
hayabusata <falcon.cryptomeria@gmail.com>
2021-06-18 23:17:25 +0900

    display temporary one project

--- components/Project.tsx
+++ components/Project.tsx
@@ -6,12 +6,15 @@ const projectStyle = css`
   background-color: #ffffff;
   width: 400px;
   height: 150px;
+  box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.5);
+  margin-bottom: 20px;
 `;
 
 const projectNameStyle = css`
   font-size: 1.5em;
   padding-top: 20px;
   padding-left: 20px;
+  margin-top: 0;
 `;
 
 const projectDetailStyle = css`
