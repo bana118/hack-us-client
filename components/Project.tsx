7dc63530e38f8f30773425406cfec5f242ca22e7
hayabusata <falcon.cryptomeria@gmail.com>
2021-06-18 16:20:10 +0900

    adjusted position of characters in one project

--- components/Project.tsx
+++ components/Project.tsx
@@ -2,21 +2,49 @@ import React from "react";
 import { css } from "@emotion/react";
 import { Project } from "../interfaces/Project";
 
-const style = css`
+const projectStyle = css`
   background-color: #ffffff;
+  width: 400px;
+  height: 150px;
+`;
+
+const projectNameStyle = css`
+  font-size: 1.5em;
+  padding-top: 20px;
+  padding-left: 20px;
+`;
+
+const projectDetailStyle = css`
+  padding-left: 20px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+`;
+
+const projectLanguageStyle = css`
+  padding-left: 20px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+`;
+
+const projectStatusStyle = css`
+  padding-left: 20px;
+  margin-top: 5px;
+  margin-bottom: 5px;
 `;
 
 export const ProjectComp = ({
   id = "default",
   name = "default",
   detail = "default",
+  language = "asembly",
   status = "default",
 }: Project): JSX.Element => {
   return (
-    <div css={style}>
-      <p>{name}</p>
-      <p>{detail}</p>
-      <p>{status}</p>
+    <div css={projectStyle}>
+      <p css={projectNameStyle}>{name}</p>
+      <p css={projectDetailStyle}>{detail}</p>
+      <p css={projectLanguageStyle}>{language}</p>
+      <p css={projectStatusStyle}>{status}</p>
     </div>
   );
 };
--- interfaces/Project.ts
+++ interfaces/Project.ts
@@ -2,5 +2,6 @@ export type Project = {
   id?: string;
   name?: string;
   detail?: string;
+  language?: string;
   status?: string;
 };
