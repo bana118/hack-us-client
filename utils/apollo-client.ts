13036c30fb8a96b22a85d401890fd0304470ddd7
bana118 <bana.titech@gmail.com>
2021-06-19 19:59:52 +0900

    modify comment

--- utils/apollo-client.ts
+++ utils/apollo-client.ts
@@ -2,13 +2,13 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { tokenKeyName } from "./token-key-name";
 
-// SSRの時はcontextを用いてヘッダーにtokenをセットする
 const httpLink = createHttpLink({
   uri: `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`,
   // TODO credentials: "include"でcookieをサーバーに送れるはずだが送れないのでheadersで送る
   // credentials: "include",
 });
 
+// SSRの時はcontextを用いてヘッダーにtokenをセットする
 const authLink = setContext((_, { headers }) => {
   const token =
     typeof window !== "undefined"
