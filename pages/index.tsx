a4568e97a41bfc7bffd38f23e50b2c50c2afe3d2
bana118 <bana.titech@gmail.com>
2021-06-24 19:30:18 +0900

    modify index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Layout from "../components/Layout";
 import { MyHead } from "../components/MyHead";
-import Link from "next/link";
 import { ProjectComp } from "../components/Project";
 import { Project } from "../interfaces/Project";
 import { GetStaticProps } from "next";
@@ -12,50 +11,48 @@ type IndexPageProps = {
   item: Project[];
 };
 
-const IndexPage = ({ item }: IndexPageProps): JSX.Element => (
-  <Layout>
-    <MyHead title="Hack Us"></MyHead>
-    <Box>
-      {/* <p>おすすめプロジェクトなどを表示する予定</p> */}
-      <MyTabs labels={["New Projects", "Likes", "My Projects"]}>
-        <Grid container>
-          {item.map((x, idx) => {
-            return (
-              <Grid item xs={12} md={6} lg={4} key={idx}>
-                <ProjectComp
-                  id={x.id}
-                  name={x.name}
-                  detail={x.detail}
-                  status={x.status}
-                />
-              </Grid>
-            );
-          })}
-        </Grid>
-        <Container className="Likes">
-          <ProjectComp
-            id={item[0].id}
-            name={item[0].name}
-            detail={item[0].detail}
-            status={item[0].status}
-          />
-        </Container>
-        <Container className="myProjects">
-          <ProjectComp
-            id={item[0].id}
-            name={item[0].name}
-            detail={item[0].detail}
-            status={item[0].status}
-          />
-        </Container>
-      </MyTabs>
-
-      <Link href="/users/testId">
-        <a>ユーザーページ(仮)</a>
-      </Link>
-    </Box>
-  </Layout>
-);
+const IndexPage = ({ item }: IndexPageProps): JSX.Element => {
+  return (
+    <Layout>
+      <MyHead title="Hack Us"></MyHead>
+      <Box>
+        {/* <p>おすすめプロジェクトなどを表示する予定</p> */}
+        <MyTabs labels={["New Projects", "Likes", "My Projects"]}>
+          <Grid container>
+            {item.map((x, idx) => {
+              return (
+                <Grid item xs={12} md={6} lg={4} key={idx}>
+                  <ProjectComp
+                    id={x.id}
+                    name={x.name}
+                    detail={x.detail}
+                    status={x.status}
+                  />
+                </Grid>
+              );
+            })}
+          </Grid>
+          <Container className="Likes">
+            <ProjectComp
+              id={item[0].id}
+              name={item[0].name}
+              detail={item[0].detail}
+              status={item[0].status}
+            />
+          </Container>
+          <Container className="myProjects">
+            <ProjectComp
+              id={item[0].id}
+              name={item[0].name}
+              detail={item[0].detail}
+              status={item[0].status}
+            />
+          </Container>
+        </MyTabs>
+      </Box>
+    </Layout>
+  );
+};
 
 export default IndexPage;
 
