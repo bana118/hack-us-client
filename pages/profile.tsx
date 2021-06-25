47c5b8d71dc3000faf10a88d76adfb75cc4831a7
bana118 <bana.titech@gmail.com>
2021-06-25 23:41:02 +0900

    use context user instead of ssr in profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,55 +1,30 @@
-import { GetServerSideProps } from "next";
-import { GetUserQuery, GetUserQueryVariables } from "../types/graphql";
 import Layout from "../components/Layout";
 import { MyHead } from "../components/MyHead";
-import { apolloClient } from "../utils/apollo-client";
-import { GET_USER } from "../interfaces/User";
-import nookies from "nookies";
-import { uidKeyName } from "../utils/cookie-key-names";
 import { ProfileForm } from "../components/ProfileForm";
+import React, { useContext, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
+import Router from "next/router";
 
-type ProfilePageProps = {
-  user?: GetUserQuery["user"];
-  errors?: string;
-};
+const ProfilePage = (): JSX.Element => {
+  const { user } = useContext(AuthContext);
 
-const ProfilePage = ({ user, errors }: ProfilePageProps): JSX.Element => {
-  if (user == null || errors) {
-    return (
-      <Layout>
-        <p>
-          <span style={{ color: "red" }}>Error:</span> {errors}
-        </p>
-      </Layout>
-    );
-  }
+  useEffect(() => {
+    if (user === null) {
+      Router.push("/");
+    }
+  }, [user]);
 
   return (
-    <Layout>
-      <MyHead title="ユーザーページ" />
-      <h1>ユーザーページ</h1>
-      <ProfileForm user={user} />
-    </Layout>
+    <React.Fragment>
+      {user != null && (
+        <Layout>
+          <MyHead title="ユーザーページ" />
+          <h1>ユーザーページ</h1>
+          <ProfileForm user={user} />
+        </Layout>
+      )}
+    </React.Fragment>
   );
 };
 
 export default ProfilePage;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  try {
-    const cookies = nookies.get(context);
-    const uid = cookies[uidKeyName];
-    // TODO SSR時のclientとのcacheの共有が分からない
-    const { data } = await apolloClient.query<
-      GetUserQuery,
-      GetUserQueryVariables
-    >({
-      query: GET_USER,
-      variables: { uid: uid },
-      fetchPolicy: "no-cache",
-    });
-    return { props: { user: data.user } };
-  } catch (err) {
-    return { props: { errors: err.message } };
-  }
-};
